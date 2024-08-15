aa6fc25663d4a345535141fb4b700dc9b7c926ef
matheusalves00 <m_alves11@yahoo.com.br>
2024-08-15 19:13:24 -0300

    Update responsive layout to project area

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,37 @@
+const slider = document.querySelectorAll('.item_list');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length - 1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length - 1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
 function showMenu() {
   var myLink = document.getElementById("myLinks");
   var icon = document.getElementById("icon");
@@ -15,11 +49,15 @@ function showHamburguer(screen) {
   var link = document.getElementById("active");
   var icon = document.getElementById("icon");
   var logo = document.getElementById("logo");
+  var buttonPrev = document.getElementById("prev-button");
+  var buttonNext = document.getElementById("next-button");
 
   if (!screen.matches) { // If media query matches
     link.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
     icon.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
     logo.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
+    buttonPrev.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
+    buttonNext.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
   } else {
     logo.style.cssText = `display: none; overflow: hidden; visibility: hidden;`;
   }
@@ -32,3 +70,6 @@ showHamburguer(screen);
 screen.addEventListener("change", function() {
   showHamburguer(x);
 }); 
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
